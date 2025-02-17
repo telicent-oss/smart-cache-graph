e731ff2b13acb5f8175ad6401f8956391e960751
Paul Gallagher <paul.gallagher@telicent.io>
2025-02-17 12:37:07 +0000

    [Minor] Removing exclusion tags and adding illustrative test coverage.

--- scg-system/src/test/java/io/telicent/backup/services/TestBackupAndRestore.java
+++ scg-system/src/test/java/io/telicent/backup/services/TestBackupAndRestore.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
