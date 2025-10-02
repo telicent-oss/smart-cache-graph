aa87ce46c9c2b932f9ed52a57a414570408c907a
Rob Vesse <rob.vesse@telicent.io>
2025-10-02 15:03:17 +0100

    AuthZ dynamic policy generation (CORE-962)
    Ensures that dynamic roles and permissions based authorization policies
    for SCG /<dataset>/* endpoints are appropriately generated based upon
    the endpoints defined in the Fuseki configuration.
    
    Also adds generation of the static portion of the policies for Telicent
    added static endpoints

--- scg-system/src/main/java/io/telicent/core/auth/TelicentAuthorizationFilter.java
+++ scg-system/src/main/java/io/telicent/core/auth/TelicentAuthorizationFilter.java
@@ -1,6 +1,7 @@
 package io.telicent.core.auth;
 
 import io.telicent.smart.caches.server.auth.roles.AuthorizationResult;
+import io.telicent.smart.caches.server.auth.roles.TelicentAuthorizationEngine;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -18,17 +19,6 @@ import java.util.Objects;
 final class TelicentAuthorizationFilter implements Filter {
     private static final Logger LOGGER = LoggerFactory.getLogger(TelicentAuthorizationFilter.class);
 
-    private final ServletAuthorizationEngine authorizationEngine;
-
-    /**
-     * Creates a new authorization filter
-     *
-     * @param authorizationEngine Authorization engine that makes authorization decisions
-     */
-    TelicentAuthorizationFilter(ServletAuthorizationEngine authorizationEngine) {
-        this.authorizationEngine = Objects.requireNonNull(authorizationEngine);
-    }
-
     @Override
     public void init(FilterConfig filterConfig) {
         // No init required, initialised via constructor parameters
@@ -37,16 +27,25 @@ final class TelicentAuthorizationFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
             ServletException {
-        ServletAuthorizationContext context = new ServletAuthorizationContext((HttpServletRequest) request);
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
-        AuthorizationResult result = this.authorizationEngine.authorize(context);
+
+        // Check that the authorization engine was configured
+        ServletAuthorizationEngine authorizationEngine = (ServletAuthorizationEngine) request.getServletContext().getAttribute(
+                TelicentAuthorizationEngine.class.getCanonicalName());
+        if (authorizationEngine == null) {
+            unauthorized(httpResponse, "Authorization enabled but no Authorization Engine was configured");
+            return;
+        }
+
+        ServletAuthorizationContext context = new ServletAuthorizationContext(httpRequest);
+        AuthorizationResult result = authorizationEngine.authorize(context);
         String allReasons = StringUtils.join(result.reasons(), ", ");
         switch (result.status()) {
             case DENIED:
                 // Send a 401 Unauthorized
                 LOGGER.warn("Request to {} rejected: {}", context.request().getRequestURI(), allReasons);
-                httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                httpResponse.getWriter().write("Rejected due to servers authorization policy: " + allReasons);
+                unauthorized(httpResponse, "Rejected due to servers authorization policy: " + allReasons);
                 break;
             case ALLOWED:
                 // Success, pass request onwards
@@ -62,10 +61,15 @@ final class TelicentAuthorizationFilter implements Filter {
                 LOGGER.warn(
                         "Request to {} rejected due to authorization engine returning unexpected authorization status: {}",
                         context.request().getRequestURI(), result.status());
-                httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                httpResponse.getWriter().write("Rejected due to unknown authorization status: " + result.status());
+                unauthorized(httpResponse, "Rejected due to unknown authorization status: " + result.status());
                 break;
         }
+    }
+
+    private static void unauthorized(HttpServletResponse httpResponse, String message) throws IOException {
+        httpResponse.setContentType("text/plain");
+        httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        httpResponse.getWriter().write(message);
     }
 
 }
