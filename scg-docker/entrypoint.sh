c21551ec7b8cb0f5b872ad7e02184d7867469708
Rob Vesse <rob.vesse@telicent.io>
2025-11-28 09:45:11 +0000

    Properly fixes logging provider selector
    Turns out Log4j2's SLF4J provider was only supposed to be included in
    test scope but was incorrectly being copied into the runtime container.
    Fixed the maven dependency plugin invocation to only copy runtime scoped
    dependencies into the container.
    
    This avoids the need to explicitly set the SLF4J provider, and thus
    avoids the ClassNotFound error produced by the OpenTelemetry Agent seen
    with the previous release

--- scg-docker/entrypoint.sh
+++ scg-docker/entrypoint.sh
@@ -20,9 +20,5 @@ if [ $? -eq 0 ]; then
   fi
   JAVA_OPTIONS="-javaagent:${FUSEKI_DIR}/agents/opentelemetry-javaagent.jar ${JAVA_OPTIONS}"
 fi
-# Always enforce that Logback is out intended logging provider
-# Unfortunately Fuseki has a hard dependency on Log4j2 which we can't exclude so what logging provider gets selected
-# can vary depending on Classpath loading order if we don't explicitly set our intended provider
-JAVA_OPTIONS="-Dslf4j.provider=ch.qos.logback.classic.spi.LogbackServiceProvider ${JAVA_OPTIONS}"
 echo "java" $JAVA_OPTIONS -cp "$FUSEKI_CP" $MAIN "$@"
 exec "java" $JAVA_OPTIONS -cp "$FUSEKI_CP" $MAIN "$@"
