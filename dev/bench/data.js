395e39824752538830e1a74493eed2b7615be938
github-action-benchmark <github@users.noreply.github.com>
2026-01-12 10:22:03 +0000

    add SCG ABAC Persistent Scenario Benchmark (jmh) benchmark result for de216a089334f623ad999422c34150a95470aa08

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768213254428,
+  "lastUpdate": 1768213323859,
   "repoUrl": "https://github.com/telicent-oss/smart-cache-graph",
   "entries": {
     "ABAC Benchmark": [
@@ -1706,6 +1706,36 @@ window.BENCHMARK_DATA = {
             "value": 147.46306096447856,
             "unit": "ms/op",
             "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          }
+        ]
+      }
+    ],
+    "SCG ABAC Persistent Scenario Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Paul Gallagher",
+            "username": "TelicentPaul",
+            "email": "132362215+TelicentPaul@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "de216a089334f623ad999422c34150a95470aa08",
+          "message": "Merge pull request #340 from telicent-oss/dependabot/maven/maven-patch-group-f90304635c\n\nBump the maven-patch-group group with 6 updates",
+          "timestamp": "2026-01-07T17:33:30Z",
+          "url": "https://github.com/telicent-oss/smart-cache-graph/commit/de216a089334f623ad999422c34150a95470aa08"
+        },
+        "date": 1768213323534,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "io.telicent.core.SCGraphAbacPersistentScenarioBenchmark.benchmarkAbacSparqlSelect",
+            "value": 0.8721962461899956,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
           }
         ]
       }
