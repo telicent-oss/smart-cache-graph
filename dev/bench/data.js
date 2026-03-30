014dc13660461f90c3ca26e8c29d3f992e075ed1
github-action-benchmark <github@users.noreply.github.com>
2026-03-30 08:37:58 +0000

    add ABAC Benchmark (jmh) benchmark result for 7d3a68ac5c16e15e2f929390c99ba69aab4bc17c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774262077297,
+  "lastUpdate": 1774859878606,
   "repoUrl": "https://github.com/telicent-oss/smart-cache-graph",
   "entries": {
     "ABAC Benchmark": [
@@ -1042,6 +1042,64 @@ window.BENCHMARK_DATA = {
           {
             "name": "io.telicent.core.ABACLabelEvaluationBenchmark.benchmarkAbacQuery ( {\"labelComplexity\":\"complex\",\"tripleCount\":\"10000\"} )",
             "value": 0.11205442349637582,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Karolina",
+            "username": "karolina-telicent",
+            "email": "158044573+karolina-telicent@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "7d3a68ac5c16e15e2f929390c99ba69aab4bc17c",
+          "message": "[CORE-1192] - Extend SmartCacheGraphSink to allow routing data into named graphs based on Distribution-ID (#377)\n\n* Routing data into named graphs by SmartCacheGraphSink.\n\nData gets routed into named graphs based on DistributionId. Routing mode is set in code for now, to be changed.\n\n* Added distribution-id functionality to RDF patches.\n\nData sent via RDF patches can now be routed to named graphs as well. Includes unit tests.\n\n* Routing mode set via env var.\n\nROUTE_TO_NAME_GRAPHS environment variable introduced to set routing mode, clean up comments.\n\n* [Minor] - clean up duplicate code.\n\n* [Minor] - move targetGraph creation oustide of loops.\n\n* [Minor] - Make delete via RDF change use the named graph from Distribution-Id.",
+          "timestamp": "2026-03-27T11:30:00Z",
+          "url": "https://github.com/telicent-oss/smart-cache-graph/commit/7d3a68ac5c16e15e2f929390c99ba69aab4bc17c"
+        },
+        "date": 1774859878069,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "io.telicent.core.ABACLabelEvaluationBenchmark.benchmarkAbacQuery ( {\"labelComplexity\":\"simple\",\"tripleCount\":\"1000\"} )",
+            "value": 0.11102078896988044,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "io.telicent.core.ABACLabelEvaluationBenchmark.benchmarkAbacQuery ( {\"labelComplexity\":\"simple\",\"tripleCount\":\"10000\"} )",
+            "value": 0.11121502717197285,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "io.telicent.core.ABACLabelEvaluationBenchmark.benchmarkAbacQuery ( {\"labelComplexity\":\"medium\",\"tripleCount\":\"1000\"} )",
+            "value": 0.11001687373781117,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "io.telicent.core.ABACLabelEvaluationBenchmark.benchmarkAbacQuery ( {\"labelComplexity\":\"medium\",\"tripleCount\":\"10000\"} )",
+            "value": 0.11027872667139668,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "io.telicent.core.ABACLabelEvaluationBenchmark.benchmarkAbacQuery ( {\"labelComplexity\":\"complex\",\"tripleCount\":\"1000\"} )",
+            "value": 0.1109541848796213,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "io.telicent.core.ABACLabelEvaluationBenchmark.benchmarkAbacQuery ( {\"labelComplexity\":\"complex\",\"tripleCount\":\"10000\"} )",
+            "value": 0.10987931171743925,
             "unit": "ms/op",
             "extra": "iterations: 5\nforks: 1\nthreads: 1"
           }
