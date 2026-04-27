9ec317d5ef63443dcd92f9542bafc00c70e7ff6a
github-action-benchmark <github@users.noreply.github.com>
2026-04-27 11:06:49 +0000

    add SCG ABAC Persistent Scenario Benchmark (jmh) benchmark result for 6628fa89358176af928188f310a5a827cf152a9f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777287938701,
+  "lastUpdate": 1777288009287,
   "repoUrl": "https://github.com/telicent-oss/smart-cache-graph",
   "entries": {
     "ABAC Benchmark": [
@@ -7460,6 +7460,34 @@ window.BENCHMARK_DATA = {
           {
             "name": "io.telicent.core.SCGraphAbacPersistentScenarioBenchmark.benchmarkAbacSparqlSelect",
             "value": 0.8064392906527583,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Paul Gallagher",
+            "username": "TelicentPaul",
+            "email": "132362215+TelicentPaul@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "6628fa89358176af928188f310a5a827cf152a9f",
+          "message": "Merge pull request #387 from telicent-oss/dependabot/docker/scg-docker/telicent/telicent-java21-1.2.45\n\nBump telicent/telicent-java21 from 1.2.44 to 1.2.45 in /scg-docker",
+          "timestamp": "2026-04-23T07:33:27Z",
+          "url": "https://github.com/telicent-oss/smart-cache-graph/commit/6628fa89358176af928188f310a5a827cf152a9f"
+        },
+        "date": 1777288008644,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "io.telicent.core.SCGraphAbacPersistentScenarioBenchmark.benchmarkAbacSparqlSelect",
+            "value": 1.1245770784755433,
             "unit": "ms/op",
             "extra": "iterations: 5\nforks: 1\nthreads: 1"
           }
