d8e449d21cf8d05d29e82ccea00dd42835fb1496
github-action-benchmark <github@users.noreply.github.com>
2026-05-25 11:34:05 +0000

    add SCG Graph Scenario Benchmark (jmh) benchmark result for 41ad46de0c3e7d05b435a4ecddb9b18be23f0ed6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779708707315,
+  "lastUpdate": 1779708845434,
   "repoUrl": "https://github.com/telicent-oss/smart-cache-graph",
   "entries": {
     "ABAC Benchmark": [
@@ -6416,6 +6416,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "io.telicent.core.SCGraphScenarioBenchmark.benchmarkSparqlSelect",
             "value": 0.8041940553021366,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Paul Gallagher",
+            "username": "TelicentPaul",
+            "email": "132362215+TelicentPaul@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "41ad46de0c3e7d05b435a4ecddb9b18be23f0ed6",
+          "message": "Merge pull request #399 from telicent-oss/core_656_pause_kafka_processing_during_resotre\n\n[CORE-656] Pause kafka processing during restore operation",
+          "timestamp": "2026-05-20T14:06:48Z",
+          "url": "https://github.com/telicent-oss/smart-cache-graph/commit/41ad46de0c3e7d05b435a4ecddb9b18be23f0ed6"
+        },
+        "date": 1779708844935,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "io.telicent.core.SCGraphScenarioBenchmark.benchmarkGraphQL",
+            "value": 0.8339551816126007,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "io.telicent.core.SCGraphScenarioBenchmark.benchmarkSparqlSelect",
+            "value": 1.1611667482942707,
             "unit": "ms/op",
             "extra": "iterations: 5\nforks: 1\nthreads: 1"
           }
