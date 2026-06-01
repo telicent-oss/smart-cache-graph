c489ed519fdbec7beae9a66cdf5970a27b88ff52
github-action-benchmark <github@users.noreply.github.com>
2026-06-01 09:56:39 +0000

    add CQRS Benchmark (jmh) benchmark result for 28fcefb3f6316fbb29dfda5f74f0661c812b1f8d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780307723488,
+  "lastUpdate": 1780307799342,
   "repoUrl": "https://github.com/telicent-oss/smart-cache-graph",
   "entries": {
     "ABAC Benchmark": [
@@ -2380,6 +2380,34 @@ window.BENCHMARK_DATA = {
           {
             "name": "io.telicent.core.CQRSUpdateBenchmark.benchmarkCqrsUpdate",
             "value": 0.04165499571261444,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Paul Gallagher",
+            "username": "TelicentPaul",
+            "email": "132362215+TelicentPaul@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "28fcefb3f6316fbb29dfda5f74f0661c812b1f8d",
+          "message": "Merge pull request #406 from telicent-oss/core_1276_lifecycle_distribution_filtering\n\n[CORE-1276] When using named graph routing, provide additional filtering based upon the new distribution lifecycle.",
+          "timestamp": "2026-05-28T18:27:12Z",
+          "url": "https://github.com/telicent-oss/smart-cache-graph/commit/28fcefb3f6316fbb29dfda5f74f0661c812b1f8d"
+        },
+        "date": 1780307799000,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "io.telicent.core.CQRSUpdateBenchmark.benchmarkCqrsUpdate",
+            "value": 0.04695649520505626,
             "unit": "ms/op",
             "extra": "iterations: 5\nforks: 1\nthreads: 1"
           }
